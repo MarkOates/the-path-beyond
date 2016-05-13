cb8d942b95fa4f16a639a186012907b31bc901ca
Mark Oates <euph_boy@hotmail.com>
2016-05-13 10:34:28 -0400

    Add Inventory GUI item shells

--- iteration1/game.cpp
+++ iteration1/game.cpp
@@ -19,16 +19,79 @@ public:
 };
 
 
+class InventoryGUIInventoryToggleButton : public FGUIWidget
+{
+public:
+  InventoryGUIInventoryToggleButton(FGUIWidget *parent)
+    : FGUIWidget(parent, new FGUISurfaceAreaBox(740, 60, 100, 80))
+  {}
+  void on_click()
+  {
+    std::cout << "InventoryGUIInventoryToggleButton" << std::endl;
+  }
+};
+
+
+class InventoryGUICurrentItemShowcase : public FGUIWidget
+{
+public:
+  InventoryGUICurrentItemShowcase(FGUIWidget *parent)
+    : FGUIWidget(parent, new FGUISurfaceAreaBox(400, 300, 600, 400))
+  {}
+  void on_click()
+  {
+    std::cout << "InventoryGUIItemShowcase" << std::endl;
+  }
+};
+
+
+class InventoryGUIItemButton : public FGUIWidget
+{
+public:
+  InventoryItem *item;
+
+  InventoryGUIItemButton(FGUIWidget *parent, float x, float y)
+    : FGUIWidget(parent, new FGUISurfaceAreaBox(x, y, 80, 80))
+    , item(NULL)
+  {}
+  void on_click()
+  {
+    std::cout << "InventoryGUIItemButton" << std::endl;
+  }
+};
+
 
 class InventoryGUIScreen : public FGUIScreen
 {
 public:
+  const int NUM_INVENTORY_ITEM_BUTTONS;
+
+  // MODE 1 Widget
+  InventoryGUIInventoryToggleButton *toggle_button;
+
+  // MODE 2 Widgets
+  InventoryGUICurrentItemShowcase *current_item_showcase;
+  std::vector<InventoryGUIItemButton *> item_buttons;
+
   std::vector<InventoryItem *> items;
   int current_mode;
 
   InventoryGUIScreen(Display *display)
     : FGUIScreen(display)
-  {}
+    , NUM_INVENTORY_ITEM_BUTTONS(9)
+    , toggle_button(NULL)
+    , current_item_showcase(NULL)
+    , item_buttons()
+  {
+    // create our Inventory GUI Widgets
+    toggle_button = new InventoryGUIInventoryToggleButton(this);
+    current_item_showcase = new InventoryGUICurrentItemShowcase(this);
+    for (unsigned i=0; i<NUM_INVENTORY_ITEM_BUTTONS; i++)
+    {
+      InventoryGUIItemButton *button = new InventoryGUIItemButton(this, 700, 150+90*i);
+      item_buttons.push_back(button);
+    }
+  }
 
   void show() {}
   void hide() {}
