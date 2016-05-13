49a8e1e07a096d79ddc12fb1b73f79f81b56fbfe
Mark Oates <euph_boy@hotmail.com>
2016-05-13 19:56:44 -0400

    Add shell for World Screen

--- iteration1/game.cpp
+++ iteration1/game.cpp
@@ -8,6 +8,66 @@
 #include "start_screen.hpp"
 
 
+////////////////////////////////////////////////////////////////
+
+
+
+class WorldScreenGUINavView : public FGUIWidget
+{
+public:
+  ALLEGRO_BITMAP *render;
+
+  WorldScreenGUINavView(FGUIWidget *parent)
+    : FGUIWidget(parent, new FGUISurfaceAreaBox(SCREEN_W/2, SCREEN_H/2, SCREEN_W, SCREEN_H))
+    , render(NULL)
+  {
+    // initialize our rendering surface
+    render = al_create_bitmap(place.size.x, place.size.y);
+
+    ALLEGRO_STATE state;
+    al_store_state(&state, ALLEGRO_STATE_TARGET_BITMAP);
+    al_set_target_bitmap(render);
+    al_clear_to_color(color::skyblue);
+    al_restore_state(&state);
+  }
+};
+
+
+class WorldScreenGUINavButton : public FGUIWidget
+{
+public:
+  WorldScreenGUINavButton(FGUIWidget *parent, float x, float y, float w, float h)
+    : FGUIWidget(parent, new FGUISurfaceAreaBox(x, y, w, h))
+  {}
+};
+
+
+class WorldScreenGUIScreen : public FGUIScreen
+{
+public:
+  WorldScreenGUINavView *nav_view;
+  WorldScreenGUINavButton *nav_up_button;
+  WorldScreenGUINavButton *nav_down_button;
+  WorldScreenGUINavButton *nav_left_button;
+  WorldScreenGUINavButton *nav_right_button;
+
+  WorldScreenGUIScreen(Display *display)
+    : FGUIScreen(display)
+    , nav_view(NULL)
+    , nav_up_button(NULL)
+    , nav_down_button(NULL)
+    , nav_left_button(NULL)
+    , nav_right_button(NULL)
+  {
+    nav_view = new WorldScreenGUINavView(this);
+    nav_up_button = new WorldScreenGUINavButton(this, SCREEN_W/2, 100, 300, 30);
+    nav_down_button = new WorldScreenGUINavButton(this, SCREEN_W/2, SCREEN_H-100, 300, 30);
+    nav_left_button = new WorldScreenGUINavButton(this, 100, SCREEN_H/2, 30, 300);
+    nav_right_button = new WorldScreenGUINavButton(this, SCREEN_W-100, SCREEN_H/2, 30, 300);
+  }
+};
+
+
 
 ////////////////////////////////////////////////////////////////
 
@@ -16,11 +76,13 @@
 class Project : public FGUIScreen
 {
 public:
+  WorldScreenGUIScreen *world_screen;
   InventoryGUIScreen *inventory_screen;
   StartScreenGUIScreen *start_screen;
 
   Project(Display *display)
     : FGUIScreen(display)
+    , world_screen(new WorldScreenGUIScreen(display))
     , inventory_screen(new InventoryGUIScreen(display))
     , start_screen(new StartScreenGUIScreen(this, display))
   {
