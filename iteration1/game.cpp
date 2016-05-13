180e9466eee318589b9bdb5d06e4d17c630f0350
Mark Oates <euph_boy@hotmail.com>
2016-05-13 09:58:28 -0400

    Add basic InventoryGUIScreen shell

--- iteration1/game.cpp
+++ iteration1/game.cpp
@@ -2,13 +2,53 @@
 #include <allegro_flare/allegro_flare.h>
 
 
+
+
+class InventoryItem
+{
+public:
+  std::string name;
+  std::string description;
+  ALLEGRO_BITMAP *image;
+
+  InventoryItem(std::string name, std::string description, ALLEGRO_BITMAP *image)
+    : name(name)
+    , description(description)
+    , image(image)
+  {}
+};
+
+
+
+class InventoryGUIScreen : public FGUIScreen
+{
+public:
+  std::vector<InventoryItem *> items;
+  int current_mode;
+
+  InventoryGUIScreen(Display *display)
+    : FGUIScreen(display)
+  {}
+
+  void show() {}
+  void hide() {}
+  void set_mode(int mode) {}
+};
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
 class Project : public FGUIScreen
 {
 public:
+  InventoryGUIScreen *inventory_screen;
+
   Project(Display *display)
     : FGUIScreen(display)
-  {}
-  void primary_timer_func() override
+    , inventory_screen(new InventoryGUIScreen(display))
   {}
 };
 
