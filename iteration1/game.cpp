160c5bd81b1584c95cff897e1507e558f405b1ef
Mark Oates <euph_boy@hotmail.com>
2016-05-14 16:00:11 -0400

    Add all-powerful Script class

--- iteration1/game.cpp
+++ iteration1/game.cpp
@@ -14,6 +14,68 @@
 
 
 
+class Script : public ElementID
+{
+public:
+  static ElementIDManager *manager;
+  static WorldNavigationGUIScreen *world_navigation_gui;
+  static InventoryGUIScreen *inventory_gui;
+  static StartScreenGUIScreen *start_screen_gui;
+  static bool initialized;
+
+  static void initialize(
+      WorldNavigationGUIScreen *wnguis,
+      InventoryGUIScreen *iguis,
+      StartScreenGUIScreen *ssgui
+      )
+  {
+    if (initialized) return;
+
+    manager = new ElementIDManager();
+    world_navigation_gui = wnguis;
+    start_screen_gui = ssgui;
+    inventory_gui = iguis;
+
+    initialized = true;
+  }
+  static Script *find_by_id(std::string id)
+  {
+    ElementID *element = manager->get_element_by_id(id);
+    if (element) return static_cast<Script *>(element);
+    return NULL;
+  }
+  static bool run(std::string id)
+  {
+    Script *script = find_by_id(id);
+    if (!script)
+    {
+      std::cout << "Could not run script: Script \"" << id << "\" not foud." << std::endl;
+      return false;
+    }
+    std::cout << "{{{ Activating script \"" << script->get_id() << "\"" << std::endl;
+    script->activate();
+    std::cout << "}}} Script \"" << script->get_id() << "\" finished" << std::endl;
+    return true;
+  }
+
+  Script(std::string id) : ElementID(manager)
+  {
+    set_id(id);
+  }
+  virtual void activate() = 0;
+};
+ElementIDManager *Script::manager = NULL;
+WorldNavigationGUIScreen *Script::world_navigation_gui = NULL;
+InventoryGUIScreen *Script::inventory_gui = NULL;
+StartScreenGUIScreen *Script::start_screen_gui = NULL;
+bool Script::initialized = false;
+
+
+
+////////////////////////////////////////////////////////////////
+
+
+
 class Project : public FGUIScreen
 {
 public:
@@ -27,6 +89,7 @@ public:
     , inventory_screen(new InventoryGUIScreen(display))
     , start_screen(new StartScreenGUIScreen(this, display))
   {
+    Script::initialize(world_navigation_screen, inventory_screen, start_screen);
     world_navigation_screen->set_usability_mode(0);
     inventory_screen->set_visibility_mode(0);
     start_screen->hide(0);
@@ -37,8 +100,8 @@ public:
     std::string trigger_id;
     if (TargetID::extract_trigger_id(message, &trigger_id))
     {
-      // TODO: find and call the script here
       std::cout << "Project running script \"" << trigger_id << "\"" << std::endl;
+      Script::run(trigger_id);
     }
     else if (message == "start_game_please()") start_game();
   }
