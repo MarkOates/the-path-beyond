d44feaec100a0188888fce0b6f341f6e939a8809
Mark Oates <euph_boy@hotmail.com>
2016-05-14 17:30:25 -0400

    Add 3D rendering screen & 3d entities

--- iteration1/game.cpp
+++ iteration1/game.cpp
@@ -18,6 +18,103 @@
 ////////////////////////////////////////////////////////////////
 
 
+class Entity : public ElementID
+{
+public:
+  placement3d place;
+  placement3d velocity;
+  ModelNew *model;
+  ALLEGRO_BITMAP *texture;
+
+  Entity(ElementIDManager *manager, std::string id, ModelNew *model=NULL, ALLEGRO_BITMAP *texture=NULL)
+    : ElementID(manager)
+    , place()
+    , velocity()
+    , model(model)
+    , texture(texture)
+  {
+    set_id(id);
+    velocity.align = vec3d(0, 0, 0);
+    velocity.scale = vec3d(0, 0, 0);
+  }
+
+  void draw()
+  {
+    if (!model) return;
+    if (texture) model->set_texture(texture);
+
+    place.start_transform();
+    model->draw();
+    place.restore_transform();
+  }
+};
+
+
+
+class WorldRenderScreen : public Screen
+{
+private:
+  void primary_timer_func() override
+  {
+    update_scene();
+    draw_scene();
+  }
+
+  void set_projection(ALLEGRO_BITMAP *bitmap, ALLEGRO_TRANSFORM *t)
+  {
+    float aspect_ratio = (float)al_get_bitmap_height(bitmap) / al_get_bitmap_width(bitmap);
+    al_perspective_transform(t, -1, aspect_ratio, 1, 1, -aspect_ratio, 100);
+    al_use_projection_transform(t);
+  }
+
+public:
+  ElementIDManager *manager;
+  Entity *camera;
+
+  WorldRenderScreen(Display *display)
+    : Screen(display)
+    , manager(new ElementIDManager())
+    , camera(NULL)
+  {
+    // create the camera
+    camera = new Entity(manager, "Camera", NULL, NULL);
+  }
+
+  void update_scene()
+  {
+    for (auto &elem : manager->elements)
+    {
+      Entity *entity = static_cast<Entity *>(elem);
+      entity->place += entity->velocity;
+    }
+  }
+
+  void draw_scene()
+  {
+    // setup the render settings
+    al_set_render_state(ALLEGRO_DEPTH_TEST, 1);
+    al_set_render_state(ALLEGRO_WRITE_MASK, ALLEGRO_MASK_DEPTH | ALLEGRO_MASK_RGBA);
+    al_clear_depth_buffer(1);
+
+    ALLEGRO_TRANSFORM t;
+    camera->place.build_reverse_transform(&t);
+    set_projection(backbuffer_sub_bitmap, &t);
+
+    // draw our entities
+    for (auto &elem : manager->elements)
+    {
+      Entity *entity = static_cast<Entity *>(elem);
+      entity->draw();
+    }
+  }
+};
+
+
+
+
+////////////////////////////////////////////////////////////////
+
+
 
 class Project : public FGUIScreen
 {
