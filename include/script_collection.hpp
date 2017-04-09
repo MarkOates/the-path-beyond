0a2dc46f2eaab017ff6f99d3287e570c1b5b09cd
Mark Oates <euph_boy@hotmail.com>
2017-04-09 00:36:29 -0400

    Move ScriptCollection get_instance() into private scope

--- include/script_collection.hpp
+++ include/script_collection.hpp
@@ -9,15 +9,17 @@ class Script;
 
 class ScriptCollection : public ElementID
 {
+   friend class Script;
+
 private:
    static ElementID *instance;
+   static ElementID *get_instance();
 
 public:
    static Script *find_by_unique_id(int unique_id);
    static Script *find_by_id(std::string id);
    static void run_script(Script *script);
    static bool run(std::string id);
-   static ElementID *get_instance();
 };
 
 
