adf764ebbbca8baab91038202f4db1cd88346f89
Mark Oates <euph_boy@hotmail.com>
2017-04-12 03:31:52 -0400

    Add simple map shader to ShaderCollection

--- include/shader_collection.hpp
+++ include/shader_collection.hpp
@@ -15,9 +15,11 @@ private:
    ~ShaderCollection();
 
    Shader *flat_color_shader;
+   Shader *simple_map_shader;
 
 public:
    static Shader *get_flat_color_shader();
+   static Shader *get_simple_map_shader();
 };
 
 
--- src/shader_collection.cpp
+++ src/shader_collection.cpp
@@ -18,10 +18,12 @@ ShaderCollection *ShaderCollection::get_instance()
 
 ShaderCollection::ShaderCollection()
    : flat_color_shader(nullptr)
+   , simple_map_shader(nullptr)
 {
    if (!Framework::is_initialized()) throw std::runtime_error("Framework is not initialized");
 
    flat_color_shader = new Shader("data/shaders/flat_color_vertex.glsl", "data/shaders/flat_color_fragment.glsl");
+   simple_map_shader = new Shader("data/shaders/simple_map_vertex_with_light.glsl", "data/shaders/simple_map_fragment_with_light.glsl");
 }
 
 
@@ -29,6 +31,7 @@ ShaderCollection::ShaderCollection()
 ShaderCollection::~ShaderCollection()
 {
    delete flat_color_shader;
+   delete simple_map_shader;
 }
 
 
@@ -36,6 +39,13 @@ ShaderCollection::~ShaderCollection()
 Shader *ShaderCollection::get_flat_color_shader()
 {
    return get_instance()->flat_color_shader;
+}
+
+
+
+Shader *ShaderCollection::get_simple_map_shader()
+{
+   return get_instance()->simple_map_shader;
 }
 
 
