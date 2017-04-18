89041136962b5ff22e29996db2cd9d2db4bda582
Mark Oates <euph_boy@hotmail.com>
2017-04-17 20:53:40 -0400

    Get a WallShader from ShaderCollection

--- include/shader_collection.hpp
+++ include/shader_collection.hpp
@@ -6,6 +6,10 @@
 
 
 
+class Shader2;
+
+
+
 class ShaderCollection
 {
 private:
@@ -16,10 +20,12 @@ private:
 
    Shader *flat_color_shader;
    Shader *simple_map_shader;
+   Shader2 *wall_shader;
 
 public:
    static Shader *get_flat_color_shader();
    static Shader *get_simple_map_shader();
+   static Shader2 *get_wall_shader();
 };
 
 
--- src/shader_collection.cpp
+++ src/shader_collection.cpp
@@ -4,6 +4,7 @@
 #include <shader_collection.hpp>
 
 #include <allegro_flare/framework.h>
+#include <shaders/wall_shader.hpp>
 
 
 
@@ -19,11 +20,13 @@ ShaderCollection *ShaderCollection::get_instance()
 ShaderCollection::ShaderCollection()
    : flat_color_shader(nullptr)
    , simple_map_shader(nullptr)
+   , wall_shader(nullptr)
 {
    if (!Framework::is_initialized()) throw std::runtime_error("Framework is not initialized");
 
    flat_color_shader = new Shader("data/shaders/flat_color_vertex.glsl", "data/shaders/flat_color_fragment.glsl");
    simple_map_shader = new Shader("data/shaders/simple_map_vertex_with_light.glsl", "data/shaders/simple_map_fragment_with_light.glsl");
+   wall_shader = new WallShader(Framework::bitmap("floor_texture.jpeg"));
 }
 
 
@@ -32,6 +35,7 @@ ShaderCollection::~ShaderCollection()
 {
    delete flat_color_shader;
    delete simple_map_shader;
+   delete wall_shader;
 }
 
 
@@ -46,6 +50,13 @@ Shader *ShaderCollection::get_flat_color_shader()
 Shader *ShaderCollection::get_simple_map_shader()
 {
    return get_instance()->simple_map_shader;
+}
+
+
+
+Shader2 *ShaderCollection::get_wall_shader()
+{
+   return get_instance()->wall_shader;
 }
 
 
